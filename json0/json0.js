4693dba081c6ce885dbff2524c778ea2110d1d16
David Ko <david.ko@pvtmethod.com>
2018-01-17 12:58:23 -0500

    String transformation

--- json0/json0.js
+++ json0/json0.js
@@ -2,4 +2,11 @@ const json = require('ot-json0').type;
 
 let doc = { items: ['a', 'b', 'c'] };
 json.apply(doc, [{ p: ['items', 0], li: 'ginger' }]);
-console.log(doc);
+// 1. Use json.apply to apply transformations to data
+// 2. Pass it an array of transformation
+// 3. Each transformation has 2 attributes: p, and some transformation op
+
+let stringDoc = { blogPost: 'This is the first sentence of my blog post.' }
+json.apply(stringDoc, [{ p: ['blogPost', 5], si: 'hijacked!' }]);
+
+console.log(stringDoc);
