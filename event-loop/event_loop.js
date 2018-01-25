792dac2e69022b6c90c95638118ff9b892b2b553
David Ko <david.ko@pvtmethod.com>
2018-01-25 15:28:59 -0500

    Starving disk IO with process.nextTick

--- event-loop/event_loop.js
+++ event-loop/event_loop.js
@@ -7,4 +7,30 @@ fs.readFile('./dummy.txt', () => {
 
 setTimeout(() => {
   console.log('Timers Phase');
+  // calling process.nextTick here will
+  // cause the function to execute after the timers phase
+  // and before the IO phase
+  let start = Date.now();
+  let exit = false;
+
+  process.nextTick(() => {
+    console.log('process.nextTick is executing');
+    while (!exit) {
+      let elapsedTime = Date.now() - start;
+      if (elapsedTime > 3000) {
+        exit = true;
+      }
+    }
+    start = Date.now();
+    exit = false;
+    process.nextTick(() => {
+      console.log('2nd process.nextTick is executing');
+      while (!exit) {
+        let elapsedTime = Date.now() - start;
+        if (elapsedTime > 3000) {
+          exit = true;
+        }
+      }
+    });
+  });
 }, 0);
